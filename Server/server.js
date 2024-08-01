4e3a133307da15ab3c302fb491368cd609c4a337
Vinay Singh <vinaysthakur15@gmail.com>
2024-08-01 13:42:33 +0530

    Server side application setup

---
+++ Server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const cors = require("cors");
+app.use(cors());
+
+// express parser
+app.use(express.json());
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => console.log("DB Failed to Connect!", err));
+
+app.get("/api/health", (req, res) => {
+  console.log("hey health!");
+  res.json({
+    service: "Pocket Notes Server",
+    status: "active",
+    time: new Date(),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`PocketNotes Server is running on port ${PORT}`);
+});
